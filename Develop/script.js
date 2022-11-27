4829141082085c962af4ad173f0571df9c9a46c8
Orlando Nelson <nelson8223@gmail.com>
2022-11-26 23:54:49 -0500

    updated JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 // Assignment code here
 
 
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +16,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+generateBtn.addEventListener("click", Button);
